d0cc84cf6300f09477221bf676499f01dad9fb7e
Joshua Comeau <joshwcomeau@gmail.com>
2023-07-26 15:27:42 -0400

    Restructure file-helpers

--- src/helpers/file-helpers.js
+++ src/helpers/file-helpers.js
@@ -2,25 +2,6 @@ import fs from 'fs/promises';
 import path from 'path';
 import matter from 'gray-matter';
 
-export function readFile(localPath) {
-  return fs.readFile(
-    path.join(process.cwd(), localPath),
-    'utf8'
-  );
-}
-
-export function readDirectory(localPath) {
-  return fs.readdir(path.join(process.cwd(), localPath));
-}
-
-export async function loadBlogPost(slug) {
-  const rawContent = await readFile(`/content/${slug}.mdx`);
-
-  const { data: frontmatter, content } = matter(rawContent);
-
-  return { frontmatter, content };
-}
-
 export async function getBlogPostList() {
   const fileNames = await readDirectory('/content');
 
@@ -42,4 +23,23 @@ export async function getBlogPostList() {
   return blogPosts.sort((p1, p2) =>
     p1.publishedOn < p2.publishedOn ? 1 : -1
   );
+}
+
+export async function loadBlogPost(slug) {
+  const rawContent = await readFile(`/content/${slug}.mdx`);
+
+  const { data: frontmatter, content } = matter(rawContent);
+
+  return { frontmatter, content };
+}
+
+function readFile(localPath) {
+  return fs.readFile(
+    path.join(process.cwd(), localPath),
+    'utf8'
+  );
+}
+
+function readDirectory(localPath) {
+  return fs.readdir(path.join(process.cwd(), localPath));
 }
