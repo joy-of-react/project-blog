58adcce5e8fc01d418b52b566b80c084abf0a1ad
Joshua Comeau <joshwcomeau@gmail.com>
2023-07-29 18:16:25 -0400

    Exercise 5A solution

---
+++ src/helpers/mdx-components.js
@@ -0,0 +1,9 @@
+import CodeSnippet from '@/components/CodeSnippet';
+import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
+
+const COMPONENT_MAP = {
+  pre: CodeSnippet,
+  DivisionGroupsDemo,
+};
+
+export default COMPONENT_MAP;
