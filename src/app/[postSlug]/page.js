23e309b775c3b4583d79132d403e8f70f32eb348
Joshua Comeau <joshwcomeau@gmail.com>
2023-07-29 11:59:04 -0400

    Exercise 3 solution

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
+import { BLOG_TITLE } from '@/constants';
 import { loadBlogPost } from '@/helpers/file-helpers';
 
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
+
+export async function generateMetadata({ params }) {
+  const { frontmatter } = await loadBlogPost(
+    params.postSlug
+  );
+
+  return {
+    title: `${frontmatter.title} • ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  };
+}
 
 async function BlogPost({ params }) {
   const { frontmatter, content } =
