50fa133ec42e066cb897c4b8a6a1efeb9abe1154
Joshua Comeau <joshwcomeau@gmail.com>
2023-07-29 14:13:27 -0400

    Exercise 4 solution

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -4,6 +4,7 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 import { BLOG_TITLE } from '@/constants';
 import { loadBlogPost } from '@/helpers/file-helpers';
 
+import CodeSnippet from '@/components/CodeSnippet';
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
@@ -33,7 +34,12 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote
+          source={content}
+          components={{
+            pre: CodeSnippet,
+          }}
+        />
       </div>
     </article>
   );
