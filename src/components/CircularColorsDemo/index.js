c0ccb12d4f37f3a309193d8abaa148a456862e3c
Joshua Comeau <joshwcomeau@gmail.com>
2023-08-01 16:57:44 -0400

    Exercise 6 solution

--- src/components/CircularColorsDemo/index.js
+++ src/components/CircularColorsDemo/index.js
@@ -1,2 +1,7 @@
-export * from './CircularColorsDemo';
-export { default } from './CircularColorsDemo';
+import dynamic from 'next/dynamic';
+
+const CircularColorsDemo = dynamic(() =>
+  import('./CircularColorsDemo')
+);
+
+export default CircularColorsDemo;
