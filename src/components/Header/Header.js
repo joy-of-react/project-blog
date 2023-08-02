00f727e7f385461cf859e9845f271385529305f3
Joshua Comeau <joshwcomeau@gmail.com>
2023-08-02 18:14:10 -0400

    Exercise 7 solution

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,51 @@
+'use client';
 import React from 'react';
 import clsx from 'clsx';
 import { Rss, Sun, Moon } from 'react-feather';
+import Cookie from 'js-cookie';
+
+import {
+  COLOR_THEME_COOKIE_NAME,
+  LIGHT_TOKENS,
+  DARK_TOKENS,
+} from '@/constants';
 
 import Logo from '@/components/Logo';
 import VisuallyHidden from '@/components/VisuallyHidden';
 
 import styles from './Header.module.css';
 
-function Header({ theme, className, ...delegated }) {
+function Header({
+  initialTheme,
+  className,
+  ...delegated
+}) {
+  const [theme, setTheme] =
+    React.useState(initialTheme);
+
+  function handleToggleTheme() {
+    const newTheme =
+      theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+
+    Cookie.set(COLOR_THEME_COOKIE_NAME, newTheme, {
+      expires: 1000,
+    });
+
+    const newTokens =
+      newTheme === 'light'
+        ? LIGHT_TOKENS
+        : DARK_TOKENS;
+    const root = document.documentElement;
+
+    root.setAttribute('data-color-theme', newTheme);
+    Object.entries(newTokens).forEach(
+      ([key, value]) => {
+        root.style.setProperty(key, value);
+      }
+    );
+  }
+
   return (
     <header
       className={clsx(styles.wrapper, className)}
@@ -28,8 +66,15 @@ function Header({ theme, className, ...delegated }) {
             View RSS feed
           </VisuallyHidden>
         </button>
-        <button className={styles.action}>
-          <Sun size="1.5rem" />
+        <button
+          className={styles.action}
+          onClick={handleToggleTheme}
+        >
+          {theme === 'light' ? (
+            <Sun size="1.5rem" />
+          ) : (
+            <Moon size="1.5rem" />
+          )}
           <VisuallyHidden>
             Toggle dark / light mode
           </VisuallyHidden>
