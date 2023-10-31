0b003b094bd2d8152e64d388cf85c09f7ba371d0
Kyle Bachand <kbachand@sbcglobal.net>
2023-10-31 17:54:49 -0500

    Page now populates with all blog posts (Exercise 1).

--- src/app/page.js
+++ src/app/page.js
@@ -2,22 +2,28 @@ import React from 'react';
 
 import BlogSummaryCard from '@/components/BlogSummaryCard';
 
+import { getBlogPostList } from '@/helpers/file-helpers';
+
 import styles from './homepage.module.css';
 
-function Home() {
+async function Home() {
+  const blogPosts = await getBlogPostList();
+
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>
         Latest Content:
       </h1>
-
-      {/* TODO: Iterate over the data read from the file system! */}
+      
+      {blogPosts.map(({ slug, title, abstract, publishedOn }) => (
       <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
+        key={slug}
+        slug={slug}
+        title={title}
+        abstract={abstract}
+        publishedOn={publishedOn}
       />
+      ))}
     </div>
   );
 }
