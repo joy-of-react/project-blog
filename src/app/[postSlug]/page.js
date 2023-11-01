0684cb58fd63e77eb4284bd0c43a9667ffecffd0
Kyle Bachand <kbachand@sbcglobal.net>
2023-10-31 21:31:42 -0500

    Homepage and blog pages now have their own metadata. Tab titles showing correctly

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -6,20 +6,28 @@ import styles from './postSlug.module.css';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
+export async function generateMetadata({ params }) {
+  const { frontmatter } = await loadBlogPost(params.postSlug);
+  return {
+    title: frontmatter.title
+  };
+}
+
 async function BlogPost({ params }){
   const { frontmatter, content } = await loadBlogPost(params.postSlug);
-
+  
   return (
     <article className={styles.wrapper}>
       <BlogHero
         title={frontmatter.title}
         publishedOn={frontmatter.publishedOn}
-      />
+        />
       <div className={styles.page}>
       <MDXRemote source ={content}/>
       </div>
     </article>
   );
 }
+
 
 export default BlogPost;
--- src/app/layout.js
+++ src/app/layout.js
@@ -5,11 +5,15 @@ import {
 } from 'next/font/google';
 import clsx from 'clsx';
 
-import { LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
-
+import { BLOG_TITLE, LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './styles.css';
+
+export const metadata = {
+  title: BLOG_TITLE,
+  description: 'A wonderful blog about JavaScript',
+};
 
 const mainFont = Work_Sans({
   subsets: ['latin'],
