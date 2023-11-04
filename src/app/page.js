f62068eac86ad3aacff8de48722e09357fa4b88e
Richard Nemeth <nemethricsi@gmail.com>
2023-11-04 10:57:39 +0100

    add exercise-1

--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,25 @@ import BlogSummaryCard from '@/components/BlogSummaryCard';
 
 import styles from './homepage.module.css';
 
-function Home() {
+import { getBlogPostList } from '@/helpers/file-helpers';
+
+async function Home() {
+  const blogPosts = await getBlogPostList();
+
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>
         Latest Content:
       </h1>
-
-      {/* TODO: Iterate over the data read from the file system! */}
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
+      {blogPosts.map((post) => (
+        <BlogSummaryCard
+          key={post.slug}
+          slug={post.slug}
+          title={post.title}
+          abstract={post.abstract}
+          publishedOn={post.publishedOn}
+        />
+      ))}
     </div>
   );
 }
