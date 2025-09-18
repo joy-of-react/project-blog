8cd51876d6113fe235242a620904ba63a0adfd35
Kim Fransson <kim.fransson.22@gmail.com>
2025-09-18 08:32:00 +0200

    fix hydration error (bug) and prefetch posts

--- src/components/BlogSummaryCard/BlogSummaryCard.js
+++ src/components/BlogSummaryCard/BlogSummaryCard.js
@@ -1,37 +1,28 @@
-import React from 'react';
-import Link from 'next/link';
-import { format } from 'date-fns';
+import React from "react";
+import Link from "next/link";
+import { format } from "date-fns";
 
-import Card from '@/components/Card';
+import Card from "@/components/Card";
 
-import styles from './BlogSummaryCard.module.css';
+import styles from "./BlogSummaryCard.module.css";
 
-function BlogSummaryCard({
-  slug,
-  title,
-  publishedOn,
-  abstract,
-}) {
+function BlogSummaryCard({ slug, title, publishedOn, abstract }) {
   const href = `/${slug}`;
   const humanizedDate = format(
     new Date(publishedOn),
-    'MMMM do, yyyy'
+    "MMMM do, yyyy"
   );
 
   return (
     <Card className={styles.wrapper}>
-      <Link href={href} className={styles.title}>
+      <Link prefetch={true} href={href} className={styles.title}>
         {title}
       </Link>
       <time dateTime={publishedOn}>{humanizedDate}</time>
       <p>
-        {abstract}{' '}
-        <Link
-          href={href}
-          className={styles.continueReadingLink}
-        >
-          Continue reading{' '}
-          <span className={styles.arrow}>→</span>
+        {abstract}{" "}
+        <Link href={href} className={styles.continueReadingLink}>
+          Continue reading <span className={styles.arrow}>→</span>
         </Link>
       </p>
     </Card>
